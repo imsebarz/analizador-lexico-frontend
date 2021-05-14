7daaebb459fac4ebd268ea862d583f161b4b55a8
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2021-05-14 17:29:49 -0500

    fixed warning

--- src/Token.jsx
+++ src/Token.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Token = ({ id, type, literal }) => {
   return (
-    <div className={`token ${type == "ILLEGAL" ? "illegal" : ""}`}>
+    <div className={`token ${type === "ILLEGAL" ? "illegal" : ""}`}>
       <p>
         {" "}
         <strong> {id} . Literal: </strong>
